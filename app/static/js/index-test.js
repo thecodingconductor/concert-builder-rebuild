0f42e5cba4e3b4caa588e73cce9d2c7a4644574f
Tristan <tristan.raissherman@gmail.com>
2020-07-30 16:27:15 -0400

    went back to using the XMLHttpResponse()

--- app/static/js/index-test.js
+++ app/static/js/index-test.js
@@ -16,27 +16,48 @@ function clearList() {
     }
 }
 
-async function getResults() {
-    console.log('within getResult()>>>', searchField.value);
-    const rawResponse = await fetch('/composers', {
-        method: 'POST',
-        headers: {
-            'Accept': 'application/json',
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(searchField.value)
-    });
 
-    const content = await rawResponse.json();
-    console.log(content);   
+function getResults() {
+    const request = new XMLHttpRequest();
+    const search = document.querySelector("#search-field").value;
+            
+    request.open('POST', '/composers');
+
+    //handle the JSON response from Flask
+    request.onload = () => {
+
+        const data = JSON.parse(request.response);
+                
+        //if request was successful, create list of links
+        if (data.success) {
+                
+            const composer_data = JSON.parse(data.composers);
+                  
+            for (let i=0; i < composer_data.length; i++) {
+                const li = document.createElement('li');
+                li.innerHTML = `<a class="composerLinks" href="composer/${composer_data[i].name}">${composer_data[i].name}</a>`; 
+                document.querySelector("#composer-results").append(li);
+                }
+            }
+            else {
+                document.querySelector("#test").innerHTML = 'There was an error.';
+            };             
+        };
+
+            
+
+    const data = new FormData();
+    data.append('search-field', search);
+
+    request.send(data);
+    return false;
+
 }
-
 
 //event listeners
 
 
 searchField.addEventListener('keyup', () => {
-    
     
     clearList();
     getResults();
