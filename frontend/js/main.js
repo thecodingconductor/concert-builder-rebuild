9c4e179c125c1e479982eb780a4c4dc9c1d3c1e0
Tristan <tristan.raissherman@gmail.com>
2020-08-04 17:59:33 -0400

    fix mouse enter and leave

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,5 +1,7 @@
 const loggedIn = document.getElementById('logged-in');
 const dropDownMenu = document.getElementById('dropdown-menu');
+const showGold = document.querySelectorAll('.hover-gold');
+const searchResults = document.querySelectorAll('.search-result');
 
 function showDropDown() {
     if(dropDownMenu.style.display === 'block') {
@@ -11,6 +13,19 @@ function showDropDown() {
     
 }
 
+function showGoldUnderline(e) {
+    console.log(e.target);
+}
+
+function hideGoldUnderline(e) {
+    console.log(e.target);
+}
 
 
-loggedIn.addEventListener('click', showDropDown)
+loggedIn.addEventListener('click', showDropDown);
+
+searchResults.forEach(result => {
+    result.addEventListener('mouseover', showGoldUnderline)
+    result.addEventListener('mouseleave', hideGoldUnderline);
+
+})
