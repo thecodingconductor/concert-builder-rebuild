e8f22506c607f10092b9b9612c9cd52e330e86e6
Tristan <tristan.raissherman@gmail.com>
2020-08-05 10:48:33 -0400

    landing hero and sign up form mobile view

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -2,6 +2,7 @@ const loggedIn = document.getElementById('logged-in');
 const dropDownMenu = document.getElementById('dropdown-menu');
 const showGold = document.querySelectorAll('.hover-gold');
 const searchResults = document.querySelectorAll('.search-result');
+const viewMore = document.querySelectorAll('.view-more-btn');
 
 function showDropDown() {
     if(dropDownMenu.style.display === 'block') {
@@ -25,10 +26,23 @@ function hideGoldUnderline(e) {
 }
 
 
+// function resultsExit(e) {
+//     e.preventDefault();
+//     searchResultsReverse = [...searchResults].reverse();
+//     searchResultsReverse.forEach(result => {
+//         result.classList.add('leave');
+//     })
+// }
+
 loggedIn.addEventListener('click', showDropDown);
 
 searchResults.forEach(result => {
     result.addEventListener('mouseover', showGoldUnderline)
     result.addEventListener('mouseleave', hideGoldUnderline);
 
-})
+});
+
+
+// viewMore.forEach(link => {
+//     link.addEventListener('click', resultsExit);
+// })
