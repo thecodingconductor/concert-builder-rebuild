35f0bf595386ce49f85873467d8564f0da3e93b8
thecodingconductor <tristan.raissherman@gmail.com>
2020-09-13 09:10:05 -0400

    concert displaying on load now.

--- app/static/js-modules/main/homepage.js
+++ app/static/js-modules/main/homepage.js
@@ -3,6 +3,7 @@ import { Forms } from '../modules/forms';
 import { HTTP } from '../modules/http';
 import { Nav } from '../modules/nav';
 import { Requests } from '../modules/requests';
+import { Storage } from '../modules/storage';
 import { UI } from '../modules/ui';
 import { UISelectors } from '../modules/UISelectors';
 import "../../css/landing.css";
@@ -39,3 +40,9 @@ UISelectors.openBrowse.addEventListener('click', UI.showBrowse);
 [...UISelectors.createConcertBtns].forEach((btn) => {
   btn.addEventListener('click', UI.createConcertFunction);
 });
+
+if (UISelectors.user !== null) {
+  const currentUser = Storage.getUser();
+  window.addEventListener('DOMContentLoaded', UI.displayConcerts(currentUser))
+}
+
