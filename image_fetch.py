cd74d5f90f9591e86dbc7d3f1a1f8bc0a17a5d09
thecodingconductor <tristan.raissherman@gmail.com>
2020-11-19 14:51:50 -0500

    whoops

--- image_fetch.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-
-res = requests.get("https://www.googleapis.com/customsearch/v1?key=AIzaSyC72emsapcuXsF64Hrn7ca_9xIbAUbn7DY&cx=014124391945830086859:nr9ypxtecba&q={}&searchType=image".format(composer.name))
-
-
